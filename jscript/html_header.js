409646ae5503083b6071cf549061298ed2229822
Josh Thibeault <jxthibeault@gmail.com>
2020-05-22 13:24:47 -0400

    Icons added, head now handled by JavaScript
    Added favicon and Apple device icon. <div> footer tag changed to <span> to better align with conventions. HTML head information migrated to JavaScript.

---
+++ jscript/html_header.js
@@ -0,0 +1,3 @@
+document.head.innerHTML = document.head.innerHTML + "<meta charset=\"utf-8\">";
+document.head.innerHTML = document.head.innerHTML + "<link rel=\"icon\" type=\"image/png\" href=\"../media/favicon.png\" />";
+document.head.innerHTML = document.head.innerHTML + "<link rel=\"apple-touch-icon\" href=\"/apple-touch-icon.png\" />";
