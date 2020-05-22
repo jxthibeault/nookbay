38341b10b509c0eabd57d79c38071577f0072313
Josh Thibeault <jxthibeault@gmail.com>
2020-05-22 10:31:42 -0400

    Replaced PHP footers with Javascript
    PHP page footers have been replaced by Javascript to a) reduce load on the server, and b) increase performance by allowing the client to cache scripts used on multiple pages

---
+++ jscript/html_footer.js
@@ -0,0 +1,7 @@
+var footer = document.getElementById("footer");
+footer.innerHTML = "<a href=\"https://nookbay.app/policy/privacy.php\">Privacy Policy</a>";
+footer.innerHTML = footer.innerHTML + "<a href=\"https://nookbay.app/policy/email_policy.php\">Email Policy</a>";
+footer.innerHTML = footer.innerHTML + "<a href=\"https://github.com/jxthibeault/nookbay/security/advisories?state=published\">Advisories</a>";
+footer.innerHTML = footer.innerHTML + "<a href=\"https://github.com/jxthibeault/nookbay/issues\">Issue Tracker</a>";
+footer.innerHTML = footer.innerHTML + "<a href=\"https://github.com/jxthibeault/nookbay\">Open Source</a>";
+footer.innerHTML = footer.innerHTML + "<br />v0.1.2a";
