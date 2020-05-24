868de672b06fe50856fbcda62089660b93a69ac5
Josh Thibeault <jxthibeault@gmail.com>
2020-05-24 10:40:59 -0400

    Apple icon size reduced, footer links fixed
    Changed apple-touch-icon for lower data usage and increased readability on smaller devices. Fixed deprecated links in HTML footer.

--- jscript/html_footer.js
+++ jscript/html_footer.js
@@ -1,8 +1,8 @@
 var footer = document.getElementById("footer");
 
 footer.innerHTML = footer.innerHTML + "<div class=\"row\">";
-footer.innerHTML = footer.innerHTML + "<div class=\"col-2\"><a href=\"https://nookbay.app/policy/privacy.php\">Privacy Policy</a></div>";
-footer.innerHTML = footer.innerHTML + "<div class=\"col-2\"><a href=\"https://nookbay.app/policy/email_policy.php\">Email Policy</a></div>";
+footer.innerHTML = footer.innerHTML + "<div class=\"col-2\"><a href=\"https://nookbay.app/policy/privacy.html\">Privacy Policy</a></div>";
+footer.innerHTML = footer.innerHTML + "<div class=\"col-2\"><a href=\"https://nookbay.app/policy/email_policy.html\">Email Policy</a></div>";
 footer.innerHTML = footer.innerHTML + "<div class=\"col-2\"><a href=\"https://github.com/jxthibeault/nookbay/security/advisories?state=published\">Advisories</a></div>";
 footer.innerHTML = footer.innerHTML + "<div class=\"col-2\"><a href=\"https://github.com/jxthibeault/nookbay/issues\">Issue Tracker</a></div>";
 footer.innerHTML = footer.innerHTML + "<div class=\"col-2\"><a href=\"https://github.com/jxthibeault/nookbay\">Open Source</a></div>";
